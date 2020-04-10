7be82da97d4450328dc2464254ffb04af5e4c3f8
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-04-10 10:00:15 -0500

    Initial vertion

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+/*
+
+PIEDRA PAPEL O TIJERA
+
+  1. preguntar si quiere jugar (queda mejor si juega una vez y luego pregunta si quiere seguir jugando)
+  2. Solicitar eleccion al jugador.
+  3. Generar eleccion aleatoria a la cpu.
+  4. Evaluar quien gano.
+  5. Imprimir quien gano.
+
+*/
