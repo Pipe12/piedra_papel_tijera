f3f89ba36449be871acf3e890a307002d88bee93
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-04-10 15:25:32 -0500

    Organize a main program

--- js/app.js
+++ js/app.js
@@ -1,38 +1,42 @@
 
 /*
-
 PIEDRA PAPEL O TIJERA
-
   1. preguntar si quiere jugar (queda mejor si juega una vez y luego pregunta si quiere seguir jugando)
   2. Solicitar eleccion al jugador.
   3. Generar eleccion aleatoria a la cpu.
   4. Evaluar quien gano. ok
   5. Imprimir quien gano.
-
 */
 
 //VARIABLES
-
 let choicePlayer
-let choiceCpu = 'papel'
+let choiceCpu
 let playAgain
+let winner
 
 // Solicitar eleccion del jugador
-
 function getPlayerChoice() {
   return (prompt(`Escriba con cual quiere jugar PIEDRA, PAPEL o TIJERA ??`))
 }
 
 // Generar eleccion CPU
-
 function generateCpuChoice() {
   let randomNumber = Math.floor(Math.random()*100)
-  console.log(randomNumber)
+
+  if(randomNumber >= 0 && randomNumber < 34) {
+    return `piedra`
+  } else if(randomNumber > 33 && randomNumber < 67) {
+    return `papel`
+  } else {
+    return `tijera`
+  }
 }
 
 //Evaluar quien gano.
-
 function whoWin(choicePlayer, choiceCpu) {
+  if(choicePlayer === choiceCpu) {
+    return `Empate`
+  }
   if(choicePlayer === 'piedra') {
     return choiceCpu === 'papel' ? 'CPU won !!!' : 'You won !!!'
   }
@@ -44,15 +48,12 @@ function whoWin(choicePlayer, choiceCpu) {
   }
 }
 
-//Comienza el juego de piedra, papel o tijera.
-
-
-
-// do {
-//   choicePlayer = getPlayerChoice()
-//   console.log(choicePlayer)
-
-//   playAgain = prompt(`Por favor digite 'y' si quiere seguir jugando o cualquier letra si desea terminar el juego.`)
-
-// } while(playAgain === 'y')
+//Programa secuencial del juego de piedra, papel o tijera.
+do {
+  choicePlayer = getPlayerChoice()
+  choiceCpu = generateCpuChoice()
+  winner = whoWin(choicePlayer, choiceCpu)
+  console.log(winner)  
+  playAgain = prompt(`Por favor digite 'y' si quiere seguir jugando o cualquier letra si desea terminar el juego.`)
+} while(playAgain === 'y')
 
